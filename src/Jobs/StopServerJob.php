a6eb595f772c6c9beaaefb56c51a741059097021
freekmurze <freekmurze@users.noreply.github.com>
2022-08-27 00:40:40 +0000

    Fix styling

--- src/Jobs/StopServerJob.php
+++ src/Jobs/StopServerJob.php
@@ -9,7 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Spatie\DynamicServers\Events\DeletingServerEvent;
 use Spatie\DynamicServers\Events\StoppingServerEvent;
 use Spatie\DynamicServers\Models\Server;
 use Spatie\DynamicServers\Support\Config;
