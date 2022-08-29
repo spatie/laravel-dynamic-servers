9df1fe8329104c7a43016929022f0c7f477dd339
Freek Van der Herten <freek@spatie.be>
2022-08-29 09:04:06 +0200

    wip

--- tests/Models/ServerTest.php
+++ tests/Models/ServerTest.php
@@ -1,19 +1,40 @@
 <?php
 
+use Spatie\DynamicServers\Enums\ServerStatus;
 use Spatie\DynamicServers\Models\Server;
 
-beforeEach(function() {
+beforeEach(function () {
     /** @var Server server */
     $this->server = Server::factory()->create();
 });
 
-it('can add meta data to a server', function(mixed $value) {
+it('can add meta data to a server', function (mixed $value) {
     $this->server->addMeta('key', $value);
 
     expect($this->server->refresh()->meta['key'])->toEqual($value);
 })->with([
     'a string',
-    ['an', 'array', 'of', 'string'],
+    ['an', 'array', 'of', 'strings'],
     true,
     1,
 ]);
+
+it('can merge meta data of a server', function () {
+    $this->server->addMeta('original_key', 'original_value');
+    $this->server->addMeta('another_key', 'another_value');
+
+    expect($this->server->refresh()->meta->toArray())->toEqual([
+        'original_key' => 'original_value',
+        'another_key' => 'another_value',
+    ]);
+});
+
+it('can mark a server as errored', function () {
+    $this->server->markAsErrored(new Exception('This is an exception'));
+
+    expect($this->server)
+        ->status->toBe(ServerStatus::Errored)
+        ->exception_class->toBe(Exception::class)
+        ->exception_message->toBe('This is an exception')
+        ->exception_trace->toBeString();
+});
