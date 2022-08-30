f9aad10dffdfbe4bcbdb7da4edf012d4aaa9e6dc
freekmurze <freekmurze@users.noreply.github.com>
2022-08-30 08:25:15 +0000

    Fix styling

--- tests/Feature/StopServerFlowTest.php
+++ tests/Feature/StopServerFlowTest.php
@@ -3,52 +3,48 @@
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Queue;
 use Spatie\DynamicServers\Enums\ServerStatus;
-use Spatie\DynamicServers\Jobs\DeleteServerJob;
 use Spatie\DynamicServers\Jobs\StopServerJob;
 use Spatie\DynamicServers\Models\Server;
 
+beforeEach(function () {
+    $serverResponse = getStub('server', [
+        'server.uuid' => 'fake-uuid',
+    ]);
 
-    beforeEach(function () {
-        $serverResponse = getStub('server', [
-            'server.uuid' => 'fake-uuid',
+    $stoppingServer = getStub('server', [
+        'server.uuid' => 'fake-uuid',
+        'server.state' => 'maintenance',
+    ]);
+
+    $stoppedServer = getStub('server', [
+        'server.uuid' => 'fake-uuid',
+        'server.state' => 'stopped',
+    ]);
+
+    Http::preventStrayRequests()
+        ->fake([
+            'https://api.upcloud.com/1.3/server' => Http::response($serverResponse),
+            'https://api.upcloud.com/1.3/server/fake-uuid/stop' => Http::response($stoppingServer),
+            'https://api.upcloud.com/1.3/server/fake-uuid' => Http::response($stoppedServer),
+            'https://api.upcloud.com/1.3/server/fake-uuid?storages=1&backups=delete' => Http::response($stoppingServer),
         ]);
 
-        $stoppingServer = getStub('server', [
-            'server.uuid' => 'fake-uuid',
-            'server.state' => 'maintenance',
-        ]);
+    Queue::fake();
 
-        $stoppedServer = getStub('server', [
-            'server.uuid' => 'fake-uuid',
-            'server.state' => 'stopped',
-        ]);
+    /** @var Server $server */
+    $this->server = Server::factory()->create([
+        'name' => 'my-server',
+        'provider' => 'up_cloud',
+        'status' => ServerStatus::Running,
+    ]);
 
+    $this->server->addMeta('server_properties', getStub('server', [
+        'server.uuid' => 'fake-uuid',
+        'server.state' => 'running',
+    ])['server']);
+});
 
-
-        Http::preventStrayRequests()
-            ->fake([
-                'https://api.upcloud.com/1.3/server' => Http::response($serverResponse),
-                'https://api.upcloud.com/1.3/server/fake-uuid/stop' => Http::response($stoppingServer),
-                'https://api.upcloud.com/1.3/server/fake-uuid' => Http::response($stoppedServer),
-                'https://api.upcloud.com/1.3/server/fake-uuid?storages=1&backups=delete' => Http::response($stoppingServer),
-            ]);
-
-        Queue::fake();
-
-        /** @var Server $server */
-        $this->server = Server::factory()->create([
-            'name' => 'my-server',
-            'provider' => 'up_cloud',
-            'status' => ServerStatus::Running,
-        ]);
-
-        $this->server->addMeta('server_properties', getStub('server', [
-            'server.uuid' => 'fake-uuid',
-            'server.state' => 'running',
-        ])['server']);
-    });
-
-it('can stop a server', function() {
+it('can stop a server', function () {
     $this->server->stop();
 
     Queue::assertPushed(StopServerJob::class);
@@ -67,6 +63,3 @@ it('can stop a server', function() {
     ray($this->server->refresh());
     expect($this->server->refresh()->status)->toBe(ServerStatus::Deleted);
 });
-
-
-
