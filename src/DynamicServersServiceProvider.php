fd16636c46181e090f4bedbb04b5677838a6c574
Freek Van der Herten <freek@spatie.be>
2022-09-07 11:36:22 +0200

    add install command

--- src/DynamicServersServiceProvider.php
+++ src/DynamicServersServiceProvider.php
@@ -9,6 +9,7 @@ use Spatie\DynamicServers\Support\Config;
 use Spatie\DynamicServers\Support\DynamicServersManager;
 use Spatie\DynamicServers\Support\ServerTypes\ServerType;
 use Spatie\DynamicServers\Support\ServerTypes\ServerTypes;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -19,21 +20,34 @@ class DynamicServersServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-dynamic-servers')
             ->hasConfigFile()
-            ->hasViews()
             ->hasMigration('create_dynamic_servers_table')
+            ->publishesServiceProvider('DynamicServersServiceProvider')
             ->hasCommands(
                 DetectHangingServersCommand::class,
                 ListDynamicServersCommand::class,
                 MonitorDynamicServersCommand::class,
-            );
+            )
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->copyAndRegisterServiceProviderInApp()
+                    ->publishMigrations()
+                    ->askToRunMigrations()
+                    ->askToStarRepoOnGitHub('spatie/laravel-dynamic-servers')
+                    ->endWith(function (InstallCommand $installCommand) {
+                        $installCommand->info('You can view all docs at https://spatie.be/docs/laravel-dynamic-servers');
+                        $installCommand->line('');
+                        $installCommand->info('Thank you very much for installing this package');
+                    });
+            });
     }
 
     public function packageRegistered()
     {
-        $this->app->singleton(DynamicServersManager::class, fn () => new DynamicServersManager());
+        $this->app->singleton(DynamicServersManager::class, fn() => new DynamicServersManager());
         $this->app->bind('dynamicServers', DynamicServersManager::class);
 
-        $this->app->singleton(ServerTypes::class, fn () => new ServerTypes());
+        $this->app->singleton(ServerTypes::class, fn() => new ServerTypes());
 
         $this->registerDefaultServerType();
     }
